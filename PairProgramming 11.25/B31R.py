a6d7f444c6d8dcf7c4c3aff49cfb6d5ec2620f32
dlraud1 <alskdjfhg0001@naver.com>
2022-11-25 12:13:34 +0900

    B31R upload

---
+++ PairProgramming 11.25/B31R.py
@@ -0,0 +1,29 @@
+# 사용자 컴퓨터 -> 인풋 1~3 31아웃
+
+import random
+
+def computer(n, mu):
+    x = random.randrange(1, 3)
+    for i in range(1, x+1):
+        print(f"컴퓨터 {n + i}") # f-string
+    n = n + x
+    if n >= mu:
+        print("computer 짐")
+        return
+    player(n, mu)
+
+def player(n, mu):
+    x = int(input("input(1~3개) = "))
+    for i in range(1, x+1):
+        print(f"플레이어 {n + i}") # f-string
+    n = n + x
+    if n >= mu:
+        print("player 짐")
+        return
+    computer(n, mu)
+
+
+bas=0
+m = int(input("몇개 완? "))
+player(bas, m)
+
